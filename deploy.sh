54be11c5832b1687f95e50c9ba80b60dbe5fcfe4
cha <cha@etsy.com>
2017-05-11 12:18:30 -0400

    also push latest

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,11 @@ if [ -z ${DOCKER_ID_USER} ]; then
 	echo "You must set DOCKER_ID_USER"
 	exit 1
 fi
+base_name="${DOCKER_ID_USER}/kafka-manager"
 
 cd kafka-manager
-./sbt -batch dist
+git pull
+./sbt -batch clean dist
 cp target/universal/kafka-manager-*.zip ..
 cd ..
 ver="$(ls | grep kafka-manager-*.zip | cut -d '-' -f 3)"
@@ -14,6 +16,7 @@ echo "VER: ${ver}"
 tag="$(basename $ver .zip)"
 echo "TAG: ${tag}"
 unzip -f "kafka-manager-${tag}.zip"
-image_name="${DOCKER_ID_USER}/kafka-manager:${tag}"
-docker build --tag "${image_name}" .
-docker push "${image_name}"
+docker build --tag "${base_name}:${tag}" .
+docker tag "${base_name}:${tag}" "${base_name}:latest" 
+docker push "${base_name}:${tag}"
+docker push "${base_name}:latest"
